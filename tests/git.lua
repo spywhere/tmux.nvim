73eedf9d8cafcbef8620f5172416a337ebfb2e3c
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2022-04-10 03:17:30 +0700

    refactor(test): improve installation experience

--- tests/git.lua
+++ tests/git.lua
@@ -2,7 +2,7 @@ vim_plug_url = 'https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.
 plugin_home = vim.fn.stdpath('cache') .. '/tmux.nvim'
 vim_plug = plugin_home .. '/plug.vim'
 
-if vim.fn.filereadable(vim_plug) ~= 0 then
+if vim.fn.filereadable(vim_plug) == 0 then
   vim.cmd(
   'silent !curl -fLo ' .. vim_plug .. ' --create-dirs ' .. vim_plug_url
   )
@@ -13,6 +13,10 @@ vim.fn['plug#begin'](plugin_home)
 vim.fn['plug#']('spywhere/tmux.nvim')
 vim.fn['plug#end']()
 
-vim.cmd('PlugInstall --sync | q')
+if vim.fn.isdirectory(plugin_home .. '/tmux.nvim') == 0 then
+  vim.cmd('PlugInstall --sync | q')
+else
+  vim.cmd('PlugUpdate --sync | q')
+end
 
 require('tmux').start()
